ef2c09768a7af46ec266b4cac06d8c1394084638
abnormity4 <glebramoshka@yandex.ru>
2026-03-24 23:01:49 +0900

    feat: add server-side avatar loader. add image optimization

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,71 +1,39 @@
-"use client"
+import { readdir } from "fs/promises";
+import path from "path";
+import SignupPage from "@/app/(auth)/signup/signup-page";
 
-import {useState} from "react";
-import {UserNameValidationErrorsProp} from "@/lib/types";
-import {USERNAME_MAX_LENGTH, USERNAME_MIN_LENGTH} from "@/lib/constants";
-import ProgressBar from "@/components/progressbar";
-import GridBackground from "@/components/grid-background";
-import Image from "next/image";
-import UsernameInput from "@/components/username-input";
-import UserAvatarGrid from "@/components/useravatar-grid";
-import {AnimatePresence, motion} from "framer-motion";
-import RegistrationNextButton from "@/components/registration-next-button";
+const validImageExtensions = [".webp", ".jpg", ".svg", ".png"];
 
-const SignUpPage = () => {
-    const [username, setUsername] = useState("Your nickname");
-    const [userAvatar, setUserAvatar] = useState("/useravatar-none.svg")
-    const [validationErrors, setValidationErrors] = useState<UserNameValidationErrorsProp>([
-        {id: "too_short", message: `Must be at least ${USERNAME_MIN_LENGTH} characters long`, passed: false},
-        {id: "too_long", message: `Must be no more than ${USERNAME_MAX_LENGTH} characters long`, passed: false}
-    ]);
-    const hasNoErrors = validationErrors.every(err => err.passed);
+const getAvatars = async () => {
+    const avatarsPath = path.join(process.cwd(), "public/avatars");
+    let files;
+    try {
+        files = await readdir(avatarsPath);
+    } catch (e) {
+        throw new Error(`Failed to read avatars folder: ${e}`);
+    }
+    for (const fileName of files) {
+        const filePath = path.join(avatarsPath, fileName);
+        const extName = path.extname(filePath);
+
+        if (!validImageExtensions.includes(extName)) {
+            throw new Error(
+                `Found a file with an unsupported type in folder: ${avatarsPath}. 
+                        Allowed types: ${validImageExtensions.join(" | ")}. 
+                        Invalid file type: ${extName}`
+            );
+        }
+    }
+
+    return files;
+}
+
+const SignupContainer = async () => {
+    const avatarUrls = await getAvatars();
 
     return (
-        <>
-                <div className="w-full mb-16 self-center md:w-1/2">
-                    <h1 className="text-lg md:text-3xl md:font-light text-neutral-200 text-center mb-3 md:mb-6 leading-7 font-google-sans-flex select-none">Let’s get you started</h1>
-                    <ProgressBar value={1} maxValue={2} />
-                </div>
-
-                <GridBackground className="flex items-center justify-center p-4 overflow-hidden ">
-
-                    <div className="flex flex-col gap-3 items-center">
-                        <div className="relative size-54 bg-red-100 border-3 border-neutral-800 rounded-full">
-                            <Image src={userAvatar} alt="Empty user avatar" fill={true} className="rounded-full" />
-                            <div className="size-8 bg-blue-400 border-2 border-neutral-800 rounded-full absolute bottom-3 right-5 z-10"></div>
-                        </div>
-                        <h2 className="[mask-image:radial-gradient(circle_at_center,black_75%,transparent_100%)] text-2xl text-center text-neutral-200 text-shadow-2xs min-h-8 w-full overflow-hidden select-none whitespace-nowrap">{username}</h2>
-
-                        <div className="flex-1 w-full">
-                            <UsernameInput
-                                setUsername={setUsername}
-                                validationErrors={validationErrors}
-                                setValidationErrors={setValidationErrors}
-                                hasNoErrors={hasNoErrors}
-                            />
-                        </div>
-
-                    </div>
-                </GridBackground>
-
-                <UserAvatarGrid setUserAvatar={setUserAvatar} />
-
-                <div className="w-full h-32 md:px-16">
-                    <AnimatePresence>
-                        {hasNoErrors && (
-                            <motion.div
-                                initial={{opacity: 0, y: -10}}
-                                animate={{opacity: 1, y: 0}}
-                                exit={{opacity: 0, y: -10}}
-                                className="flex items-center h-32"
-                            >
-                                <RegistrationNextButton />
-                            </motion.div>
-                        )}
-                    </AnimatePresence>
-                </div>
-        </>
+        <SignupPage avatarUrls={avatarUrls}/>
     )
 }
 
-export default SignUpPage;
+export default SignupContainer;
--- components/useravatar-grid.tsx
+++ components/useravatar-grid.tsx
@@ -1,41 +1,20 @@
 import Image from "next/image";
 import {Dispatch, SetStateAction} from "react";
 
-const AVATARS = [
-    "useravatar-01",
-    "useravatar-02",
-    "useravatar-03",
-    "useravatar-04",
-    "useravatar-05",
-    "useravatar-06",
-    "useravatar-07",
-    "useravatar-08",
-    "useravatar-09",
-    "useravatar-10",
-    "useravatar-11",
-    "useravatar-12",
-    "useravatar-13",
-    "useravatar-14",
-    "useravatar-15",
-    "useravatar-16",
-    "useravatar-17",
-    "useravatar-18",
-    "useravatar-19",
-    "useravatar-20",
-    "useravatar-21",
-    "useravatar-22",
-    "useravatar-23"
-]
-
-const UserAvatarGrid = ({setUserAvatar}: {setUserAvatar: Dispatch<SetStateAction<string>>}) => {
+const UserAvatarGrid = ({setUserAvatar, avatarUrls}: {setUserAvatar: Dispatch<SetStateAction<string>>, avatarUrls: string[]}) => {
     return (
         <div className="grid grid-cols-4 gap-4 p-4 self-center">
-            {AVATARS.map((avatar) => (
+            {avatarUrls.map((a) => (
                     <div
-                        key={avatar}
-                        onClick={() => setUserAvatar(`/${avatar}.webp`)}
-                        className="relative size-18 rounded-full overflow-hidden cursor-pointer">
-                        <Image src={`/${avatar}.webp`} alt="Empty user avatar" fill={true} />
+                        key={a}
+                        onClick={() => setUserAvatar(`/avatars/${a}`)}
+                        className="relative md:size-18 size-14 rounded-full overflow-hidden cursor-pointer">
+                        <Image
+                            src={`/avatars/${a}`}
+                            alt="User avatar" fill={true}
+                            loading="eager"
+                            sizes="(max-width: 768px) 25vw, 10vw"
+                        />
                     </div>
 
             ))}
