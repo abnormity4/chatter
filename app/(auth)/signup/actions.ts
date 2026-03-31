445585e2b8ca53efa124e55b1f95b4f9193c58aa
abnormity4 <glebramoshka@yandex.ru>
2026-03-31 23:56:48 +0900

    feat: hash passwords before saving to db

--- app/(auth)/signup/actions.ts
+++ app/(auth)/signup/actions.ts
@@ -2,6 +2,7 @@
 import prisma from "@/lib/prisma";
 import { z } from "zod";
 import {emailSchema, passwordSchema} from "@/lib/zodschemas";
+import bcrypt from "bcryptjs";
 
 export const createUser = async (form: {email: string, password: string}) => {
     const userSchema = z.object({
@@ -14,14 +15,16 @@ export const createUser = async (form: {email: string, password: string}) => {
     if (!validationResult.success) {
         throw new Error("Validation failed")
     } else {
-        const user = await getUserByEmail(form.email)
+        const user = await getUserByEmail(validationResult.data.email);
 
-        if (user) throw Error(`User with email ${form.email} already exists`)
+        if (user) throw Error(`User with email ${validationResult.data.email} already exists`);
+
+        const hash = await bcrypt.hash(validationResult.data.password, 10);
 
         await prisma.user.create({
             data: {
-                email: form.email,
-                password: form.password
+                email: validationResult.data.email,
+                password: hash
             }
         })
     }
