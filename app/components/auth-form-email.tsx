41754022a20a921f003cd0568698a4d9f365693d
abnormity4 <glebramoshka@yandex.ru>
2026-04-13 14:55:50 +0900

    fix: race condition in auth-form-email component

--- app/components/auth-form-email.tsx
+++ app/components/auth-form-email.tsx
@@ -1,7 +1,6 @@
 import { useState, useRef, ChangeEvent } from 'react';
 import FormField from '@/components/form-field';
 import { debounce } from '@/lib/utils';
-import { emailSchema } from '@/app/schemas';
 import type { FormFieldUIStatus } from './auth-form-types';
 import { useAuthFormContext } from './auth-form';
 import { isEmailValid } from '@/app/schemas';
@@ -14,61 +13,100 @@ const AuthFormEmail = () => {
     message: null,
   });
 
-  const resetValidationState = () => {
-    setEmailForm({ status: 'neutral', message: null });
-    setFormValidation((prev) => ({ ...prev, email: false }));
+  const formStatus = {
+    loading() {
+      setEmailForm({ status: 'loading', message: null });
+    },
+    validationFailed() {
+      setEmailForm({
+        status: 'error',
+        message: 'Please enter a valid email address.',
+      });
+    },
+    mailAlreadyInUse() {
+      setEmailForm({ status: 'error', message: 'Email is already in use.' });
+    },
+    tooManyRequests() {
+      setEmailForm({
+        status: 'error',
+        message: 'Too many attempts. Please try again later.',
+      });
+    },
+    reset() {
+      setEmailForm({ status: 'neutral', message: null });
+      setFormValidation((prev) => ({ ...prev, email: false }));
+    },
+    markAsValid() {
+      setEmailForm({ status: 'success', message: null });
+      setFormValidation((prev) => ({ ...prev, email: true }));
+    },
   };
 
-  const markAsValid = () => {
-    setEmailForm({ status: 'success', message: null });
-    setFormValidation((prev) => ({ ...prev, email: true }));
-  };
+  const abortController = useRef<AbortController | null>(null);
 
-  const debouncedValidation = useRef(
+  const debounceValidation = useRef(
     debounce(async (value: string) => {
-      const validatedEmail = isEmailValid(value);
-      if (!validatedEmail) {
-        setEmailForm({
-          status: 'error',
-          message: 'Please enter a valid email address.',
-        });
-        setFormValidation((prev) => ({ ...prev, email: false }));
+      const validationResult = isEmailValid(value);
+
+      if (!validationResult) {
+        formStatus.validationFailed();
+        return;
       }
 
-      if (formMode === 'signup') {
-        const result = await fetch('/api/auth/check-availability', {
+      if (formMode !== 'signup') {
+        formStatus.markAsValid();
+        return;
+      }
+
+      try {
+        formStatus.loading();
+        const controller = new AbortController();
+        abortController.current = controller;
+
+        const emailAvailability = await fetch('/api/auth/check-availability', {
           method: 'POST',
           body: JSON.stringify({ email: value }),
-        }).then((res) => res.json()); //TODO: fix race condition (abortcontroller)
+          signal: controller.signal,
+        });
+        const emailAvailabilityBody = await emailAvailability.json();
 
-        if (!result.success) {
-          setEmailForm({
-            status: 'error',
-            message: 'Email is already in use.',
-          });
-          setFormValidation((prev) => ({ ...prev, email: false }));
+        if (emailAvailability.status === 429) {
+          formStatus.tooManyRequests();
           return;
         }
-        return; //TODO: refactor component
+
+        if (!emailAvailabilityBody.success) {
+          formStatus.mailAlreadyInUse();
+          return;
+        }
+      } catch (err) {
+        const error = err as DOMException;
+        if (error.name === 'AbortError') {
+          return;
+        }
+
+        formStatus.validationFailed();
+        return;
       }
 
-      markAsValid();
+      formStatus.markAsValid();
     }, 500),
   );
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
-    resetValidationState();
+    abortController.current?.abort();
+    formStatus.reset();
 
     setFormData((prev) => ({ ...prev, email: value }));
 
     if (value === '') {
-      debouncedValidation.current.cancel();
+      debounceValidation.current.cancel();
       return;
     }
 
-    debouncedValidation.current(value);
+    debounceValidation.current(value);
   };
 
   return (
